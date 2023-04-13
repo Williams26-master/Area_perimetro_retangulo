2d933ae065205204d78a9faf9ae94606873b46ef
Willian Carvalho <72278301+Williams26-master@users.noreply.github.com>
2023-04-13 16:14:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let base = parseFloat(prompt("Digite o valor da base do retangulo"));
+let altura = parseFloat(prompt("Digite o valor da altura do retangulo"));
+
+let aerea = base * altura
+let perimetro = base + altura * 4
+
+document.write(`A area do retangulo é igual a: ${aerea}</br>`);
+document.write(`O perimetro do retangulo é igual a: ${perimetro}`)
+
+console.log(aerea)
+console.log(perimetro)
+
